8a5d69792a2b15ecfdbc0b9383590691d04680c1
Sami Sarwar <sa7111gh-s@student.lu.se>
2025-07-08 15:59:48 +0200

    bag 21.

---
+++ code/TrustLock/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_TrustLock_FILE C:/Users/Admin/Documents/TrustLock/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/TrustLock.exe)
+set(__QT_DEPLOY_TARGET_TrustLock_RUNTIME_DLLS C:/Qt/Qt5.14.2/6.7.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/Qt5.14.2/6.7.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/Qt5.14.2/6.7.1/llvm-mingw_64/bin/Qt6Core.dll)
